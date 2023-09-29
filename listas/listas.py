6c85d19c2671a721effd2f0a6c23349c794e681c
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-09-29 17:47:04 -0300

    Create listas.py

---
+++ listas/listas.py
@@ -0,0 +1,30 @@
+# Criar uma lista
+planets = [
+    "Mercury",
+    "Venus",
+    "Earth",
+    "Mars",
+    "Jupiter",
+    "Saturn",
+    "Uranus",
+    "Neptune",
+]
+print("The first planet is", planets[0])
+print("The second planet is", planets[1])
+print("The third planet is", planets[2])
+
+# Alterar uma lista
+planets[3] = "Red Planet"
+print("Mars is also known as", planets[3])
+
+print(planets)
+
+# Para obter o comprimento de uma lista, use a função interna len()
+number_of_planets = len(planets)
+print("There are", number_of_planets, "planets in the solar system.")
+
+# Para adicionar um item a uma lista, use o método .append(value)
+planets.append("Pluto")
+number_of_planets = len(planets)
+print("There are actually", number_of_planets, "planets in the solar system.")
+print(planets)
