b887d3434fbe08aed5d7abe10deabf0c4bf1141f
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-10-02 10:10:52 -0300

    Continuação listas

--- listas/listas.py
+++ listas/listas.py
@@ -43,3 +43,59 @@ print("The penultimate planet is", planets[-2])
 
 jupiter_index = planets.index("Jupiter")
 print("Jupiter is the", jupiter_index + 1, "planet from the sun")
+
+
+# Armazenar números em listas
+
+gravity_on_earth = 1.0
+gravity_on_the_moon = 0.166
+
+gravity_on_planets = [0.378, 0.907, 1, 0.377, 2.36, 0.916, 0.889, 1.12]
+
+bus_weight = 124054  # in Newtons, on Earth
+
+print("On Earth, a double-decker bus weighs", bus_weight, "N")
+print("On Mercury, a double-decker bus weighs", bus_weight * gravity_on_planets[0], "N")
+
+# A função max() retorna o maior número e a função min() retorna o menor número existente na lista
+
+print(
+    "The lightest a bus would be in the solar system is",
+    bus_weight * min(gravity_on_planets),
+    "N",
+)
+print(
+    "The heaviest a bus would be in the solar system is",
+    bus_weight * max(gravity_on_planets),
+    "N",
+)
+
+# Fatiar listas
+
+planets_before_earth = planets[0:2]
+print(planets_before_earth)
+
+planets_after_earth = planets[3:8]
+print(planets_after_earth)
+
+# Vai até o último elemento da lista
+planets_after_earth = planets[3:]
+print(planets_after_earth)
+
+# Junção de listas
+
+amalthea_group = ["Metis", "Adrastea", "Amalthea", "Thebe"]
+galilean_moons = ["Io", "Europa", "Ganymede", "Callisto"]
+
+regular_satellite_moons = amalthea_group + galilean_moons
+print("The regular satellite moons of Jupiter are", regular_satellite_moons)
+
+# Classificar listas - Para classificar uma lista, use o método .sort() sobre a lista. O Python classifica uma lista de cadeias de caracteres em ordem alfabética e uma lista de números em ordem numérica
+
+regular_satellite_moons.sort()
+print("The regular satellite moons of Jupiter are", regular_satellite_moons)
+
+# Ordem reversa
+
+regular_satellite_moons.sort(reverse=True)
+print("The regular satellite moons of Jupiter are", regular_satellite_moons)
